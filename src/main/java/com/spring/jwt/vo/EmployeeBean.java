0bf8f2e222ea6fe8179a59d6e9250d9ae84329e2
Varghees Samraj <varghees@netphenix.com>
2019-11-26 11:12:48 +0530

    Sample JWT Based project

---
+++ src/main/java/com/spring/jwt/vo/EmployeeBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.jwt.vo;
+
+/**
+ *
+ * @author samraj
+ */
+public class EmployeeBean {
+
+    private Integer id;
+    private String empName;
+    private String empId;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
